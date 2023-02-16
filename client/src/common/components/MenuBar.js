a659c8c487178fb3380349fd8ef5db5a73907d9d
hjl1013 <hjl1013@snu.ac.kr>
2023-02-17 00:57:13 +0900

    working on profile...

---
+++ client/src/common/components/MenuBar.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import './MenuBar.css'
+
+import HomeIcon from '@mui/icons-material/Home';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import PeopleIcon from '@mui/icons-material/People';
+import TvIcon from '@mui/icons-material/Tv';
+import DirectionsCarIcon from '@mui/icons-material/DirectionsCar';
+import SettingsIcon from '@mui/icons-material/Settings';
+
+function MenuBar() {
+    return (
+        <div className='menuBar'>
+            <div className='menuBar__item'>
+                <HomeIcon />
+                <h4>Home</h4>
+            </div>
+            <div className='menuBar__item'>
+                <AccountCircleIcon />
+                <h4>Profile</h4>
+            </div>
+            <div className='menuBar__item'>
+                <PeopleIcon />
+                <h4>Community</h4>
+            </div>
+            <div className='menuBar__item'>
+                <TvIcon />
+                <h4>Entertainment</h4>
+            </div>
+            <div className='menuBar__item'>
+                <DirectionsCarIcon />
+                <h4>Controls</h4>
+            </div>
+            <div className='menuBar__item'>
+                <SettingsIcon />
+                <h4>Settings</h4>
+            </div>
+        </div>
+    )
+}
+
+export default MenuBar
--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import './Profile.css'
+import MenuBar from '../../common/components/MenuBar'
+import ProfileBar from './components/ProfileBar'
 
 function Profile() {
     return (
         <div className='profile'>
-            <div className='profile__right'>
-                
+            <div className='profile__left'>
+                <MenuBar />
+            </div>
+
+            <div className='profile_right'>
+                <ProfileBar />
             </div>
         </div>
     )
