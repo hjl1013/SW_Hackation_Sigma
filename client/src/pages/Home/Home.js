44b6094a94df285b5885261866b535d90278214f
jollyjelly3449 <samsam0510@snu.ac.kr>
2023-02-17 03:50:22 +0900

    added map and adjusted several css features in map and menu bar

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import './Home.css'
+
 import MenuBar from '../../common/components/MenuBar'
+import Map from './components/Map'
 
 function Home() {
     return (
@@ -7,6 +10,16 @@ function Home() {
             <div className='home__menuBar'>
                 <MenuBar />
             </div>
+
+            <div className='home__map'>
+                <Map />
+            </div>
+
+            <div className='home__floating'>
+                {/* Favorites */}
+                {/* Chatting */}
+            </div>
+
         </div>
     )
 }
