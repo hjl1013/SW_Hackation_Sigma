d3841d9a7d6caa98c8b765bc9dd903ba7e21284f
jollyjelly3449 <samsam0510@snu.ac.kr>
2023-02-19 14:54:02 +0900

    updating several changes

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,9 @@ import AppRouter from './pages/Router/Router';
 
 function App() {
   return (
-    // <NavermapsProvider
-    //   ncpClientId='zbboljby1v'
-    // >
       <div className="app">
         <AppRouter />
       </div>
-    // </NavermapsProvider>
   );
 }
 
