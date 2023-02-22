f841af6ca80d91011b833a1aa1b0b331c246a3b8
hjl1013 <hjl1013@snu.ac.kr>
2023-02-22 23:14:37 +0900

    프로필 깨짐 수정, Readme 파일 수정

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -21,7 +21,7 @@ async function main() {
                     avatar: {
                         create: {
                             characterImgUrl: 'https://media.istockphoto.com/id/1138879772/ko/%EB%B2%A1%ED%84%B0/%EC%8B%9C%EA%B7%B8%EB%A7%88-%EA%B7%B8%EB%A6%AC%EC%8A%A4%EC%96%B4-%EB%AC%B8%EC%9E%90-%EC%95%84%EC%9D%B4%EC%BD%98.jpg?s=612x612&w=0&k=20&c=YqLJqDQgd7u-bKc9vgKhYIIT1rPq-U4eYZTqIZyc5s8=',
-                            carImgUrl: 'https://file.newswire.co.kr/data/datafile2/data/2021/07/3554238800_20210709103022_2517669736.jpg',
+                            carImgUrl: 'https://img.freepik.com/free-vector/rally-car-decal-graphic-wrap_210772-3473.jpg?size=626&ext=jpg',
                         },
                     },
                 },
@@ -72,7 +72,7 @@ async function main() {
                     name: '라이언',
                     avatar: {
                         create: {
-                            characterImgUrl: 'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https:%2F%2Fblog.kakaocdn.net%2Fdn%2FCMiJA%2Fbtq2BDv5tjW%2FBUPkwyuk0Ib082b8j8QAwK%2Fimg.png',
+                            characterImgUrl: 'https://i.pinimg.com/originals/a8/8b/0d/a88b0d372e9fa4f0936952d09fc92d85.png',
                             carImgUrl: '',
                         },
                     },
@@ -115,7 +115,7 @@ async function main() {
         data: {
             commuProfileImgUrl: "https://www.divido.com/wp-content/uploads/2022/08/Trip-of-a-lifetime-illustration-Divido.png",
             commuName: "여행 가즈아",
-            commuIntro: "전국 최대 규모 여행 커뮤니티",
+            commuIntro: "전국 최대 규모 여행 커뮤",
             commuHT: "#여행",
             commuMemberNumber: "32m",
         },
@@ -150,21 +150,21 @@ async function main() {
         },
     })
 
-    // create commuTheme(테마 파크_commuId:1_4)
-    await prisma.commuTheme.create({
-        data: {
-            commuId: 1,
-            commuThemeName: "랜드마크",
-            commuThemeIconUrl: "https://cdn-icons-png.flaticon.com/512/5269/5269032.png",
-        },
-    })
-
-    // create commuTheme(랜드마크_commuId:1_3)
+    // create commuTheme(테마파크_commuId:1_3)
     await prisma.commuTheme.create({
         data: {
             commuId: 1,
             commuThemeName: "테마 파크",
             commuThemeIconUrl: "https://cdn-icons-png.flaticon.com/512/7758/7758774.png",
+        },
+    })
+
+    // create commuTheme(랜드마크_commuId:1_4)
+    await prisma.commuTheme.create({
+        data: {
+            commuId: 1,
+            commuThemeName: "랜드마크",
+            commuThemeIconUrl: "https://cdn-icons-png.flaticon.com/512/5269/5269032.png",
         },
     })
 
@@ -194,9 +194,24 @@ async function main() {
             commuThemeIconUrl: "https://cdn-icons-png.flaticon.com/512/2265/2265049.png",
         },
     })
-    
-    // posting(user2_theme1(산)_1)
+
+    // posting(user3_theme1(산)_1)
     await prisma.post.create({
+        data: {
+            userId: 3,
+            commuThemeId: 1,
+            ImgUrl: "https://t1.daumcdn.net/cfile/blog/9919B9415D80B2222C",
+            title: "산골에서 느끼는 지리산의 자연",
+            text: "지리산 산골에 머물며 시원한 공기와 자연 속에서 여유로운 시간을 보내세요. 새소리와 개울 소리가 귀에 쏙쏙 들어와 마음이 편안해집니다.",
+            numberOfHearts: 31,
+            locationName: "지리산로 528",
+            locationLatitude: 35.3630134,
+            locationLongitude: 127.5632079,
+        },
+    })
+
+   // posting(user2_theme1(산)_2)
+   await prisma.post.create({
         data: {
             userId: 2,
             commuThemeId: 1,
@@ -210,7 +225,37 @@ async function main() {
         },
     })
 
-    // posting(user2_theme2(바다)_2)
+    // posting(user4_theme1(산)_3)
+    await prisma.post.create({
+        data: {
+            userId: 4,
+            commuThemeId: 1,
+            ImgUrl: "https://youimg1.c-ctrip.com/target/10081f000001gqifd7BFE_C_800_10000.jpg",
+            title: "오대산 자연휴양림에서 가족과 함께하는 봄나들이",
+            text: "오대산 자연휴양림에서 봄을 느껴보세요. 가족들과 함께 하이킹을 즐기며 꽃구경도 할 수 있습니다. 이국적인 풍경을 보면서 힐링을 느낄 수 있습니다.",
+            numberOfHearts: 64,
+            locationName: "오대산로 2",
+            locationLatitude: 37.7026578,
+            locationLongitude: 128.6021654,
+        },
+    })
+
+    // posting(user4_theme2(바다)_4)
+    await prisma.post.create({
+        data: {
+            userId: 4,
+            commuThemeId: 2,
+            ImgUrl: "https://t1.daumcdn.net/cfile/tistory/2147934D557540E929",
+            title: "부산의 멋진 해안도로를 따라 여행하기",
+            text: "부산의 멋진 해안도로를 따라 여행을 즐겨보세요. 바다와 산이 어우러진 멋진 풍경을 감상하면서 여행의 기억을 만들어보세요.",
+            numberOfHearts: 139,
+            locationName: "광복로 100",
+            locationLatitude: 36.8274658,
+            locationLongitude: 128.6293608,
+        },
+    })
+
+    // posting(user2_theme2(바다)_5)
     await prisma.post.create({
         data: {
             userId: 2,
@@ -222,51 +267,6 @@ async function main() {
             locationName: "상우목길 36-3",
             locationLatitude: 33.5051164,
             locationLongitude: 126.9483644,
-        },
-    })
-
-    // posting(user2_theme3(테마 파크)_3)
-    await prisma.post.create({
-        data: {
-            userId: 2,
-            commuThemeId: 3,
-            ImgUrl: "https://photo.sentv.co.kr/photo/news/2019/04/02/1554166522.png",
-            title: "에버랜드에서 재미있는 하루 보내기",
-            text: "에버랜드에서 재미있는 하루를 보내보세요. 다양한 놀이기구와 아름다운 꽃과 신나는 뮤지컬, 그리고 맛있는 음식도 즐길 수 있습니다.",
-            numberOfHearts: 95,
-            locationName: "에버랜드로 199",
-            locationLatitude: 37.2933272,
-            locationLongitude: 127.2013221,
-        },
-    })
-
-    // posting(user2_theme4(랜드마크)_4)
-    await prisma.post.create({
-        data: {
-            userId: 2,
-            commuThemeId: 4,
-            ImgUrl: "https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_1295,h_862,f_auto/w_80,x_15,y_15,g_south_west,l_klook_water/activities/qwo9klmec2tcooqke60l/%EA%B2%BD%EB%B3%B5%EA%B6%81%EA%B0%80%EC%9D%B4%EB%93%9C%ED%88%AC%EC%96%B4.jpg",
-            title: "경복궁에서 한국 역사 체험하기",
-            text: "경복궁에서 한국 역사를 체험해보세요. 아름다운 건물과 정원을 구경하면서 한국의 역사와 문화를 배울 수 있습니다.",
-            numberOfHearts: 180,
-            locationName: "사직로 161",
-            locationLatitude: 37.5788408,
-            locationLongitude: 126.9770162,
-        },
-    })
-
-    // posting(user3_theme1(산)_5)
-    await prisma.post.create({
-        data: {
-            userId: 3,
-            commuThemeId: 1,
-            ImgUrl: "https://t1.daumcdn.net/cfile/blog/9919B9415D80B2222C",
-            title: "산골에서 느끼는 지리산의 자연",
-            text: "지리산 산골에 머물며 시원한 공기와 자연 속에서 여유로운 시간을 보내세요. 새소리와 개울 소리가 귀에 쏙쏙 들어와 마음이 편안해집니다.",
-            numberOfHearts: 31,
-            locationName: "지리산로 528",
-            locationLatitude: 35.3630134,
-            locationLongitude: 127.5632079,
         },
     })
 
@@ -285,7 +285,22 @@ async function main() {
         },
     })
 
-    // posting(user3_theme3(테마 파크)_7)
+    // posting(user2_theme3(테마 파크)_7)
+    await prisma.post.create({
+        data: {
+            userId: 2,
+            commuThemeId: 3,
+            ImgUrl: "https://photo.sentv.co.kr/photo/news/2019/04/02/1554166522.png",
+            title: "에버랜드에서 재미있는 하루 보내기",
+            text: "에버랜드에서 재미있는 하루를 보내보세요. 다양한 놀이기구와 아름다운 꽃과 신나는 뮤지컬, 그리고 맛있는 음식도 즐길 수 있습니다.",
+            numberOfHearts: 95,
+            locationName: "에버랜드로 199",
+            locationLatitude: 37.2933272,
+            locationLongitude: 127.2013221,
+        },
+    })
+
+    // posting(user3_theme3(테마 파크)_8)
     await prisma.post.create({
         data: {
             userId: 3,
@@ -300,52 +315,7 @@ async function main() {
         },
     })
 
-    // posting(user3_theme4(랜드마크)_8)
-    await prisma.post.create({
-        data: {
-            userId: 3,
-            commuThemeId: 4,
-            ImgUrl: "http://www.k-heritage.tv/jnrepo/mig/upload/itm/contents/12227/%EA%B2%BD%EC%A3%BC%20%EB%B6%88%EA%B5%AD%EC%82%AC.jpg",
-            title: "불국사에서 고즈넉한 시간 보내기",
-            text: "불국사에서 고즈넉한 시간을 보내보세요. 아름다운 사찰과 국보급 유물을 감상하며 마음을 정화해보세요.",
-            numberOfHearts: 178,
-            locationName: "경강로 2591",
-            locationLatitude: 37.7733758,
-            locationLongitude: 128.9427571,
-        },
-    })
-
-    // posting(user4_theme1(산)_9)
-    await prisma.post.create({
-        data: {
-            userId: 4,
-            commuThemeId: 1,
-            ImgUrl: "https://youimg1.c-ctrip.com/target/10081f000001gqifd7BFE_C_800_10000.jpg",
-            title: "오대산 자연휴양림에서 가족과 함께하는 봄나들이",
-            text: "오대산 자연휴양림에서 봄을 느껴보세요. 가족들과 함께 하이킹을 즐기며 꽃구경도 할 수 있습니다. 이국적인 풍경을 보면서 힐링을 느낄 수 있습니다.",
-            numberOfHearts: 64,
-            locationName: "오대산로 2",
-            locationLatitude: 37.7026578,
-            locationLongitude: 128.6021654,
-        },
-    })
-
-    // posting(user4_theme2(바다)_10)
-    await prisma.post.create({
-        data: {
-            userId: 4,
-            commuThemeId: 2,
-            ImgUrl: "https://t1.daumcdn.net/cfile/tistory/2147934D557540E929",
-            title: "부산의 멋진 해안도로를 따라 여행하기",
-            text: "부산의 멋진 해안도로를 따라 여행을 즐겨보세요. 바다와 산이 어우러진 멋진 풍경을 감상하면서 여행의 기억을 만들어보세요.",
-            numberOfHearts: 139,
-            locationName: "광복로 100",
-            locationLatitude: 36.8274658,
-            locationLongitude: 128.6293608,
-        },
-    })
-
-    // posting(user4_theme3(테마 파크)_11)
+    // posting(user4_theme3(테마 파크)_9)
     await prisma.post.create({
         data: {
             userId: 4,
@@ -360,7 +330,52 @@ async function main() {
         },
     })
 
-    // posting(user4_theme4(랜드 마크)_12)
+    // posting(user1_theme4(랜드마크)_10)
+    await prisma.post.create({
+        data: {
+            userId: 1,
+            commuThemeId: 4,
+            ImgUrl: "https://tse2.mm.bing.net/th?id=OIP.5rbcYgrB7xbajtWluYU1OAHaHa&pid=Api&P=0",
+            title: "한국의 대표적인 랜드마크, 남산서울타워",
+            text: "남산서울타워는 한국에서 가장 높은 타워 중 하나입니다. 낮과 밤 모두 아름다운 경관을 감상할 수 있으며, 전망대에서 서울의 도시scape를 볼 수 있습니다.",
+            numberOfHearts: 0,
+            locationName: "남산공원길 105",
+            locationLatitude: 37.5510548,
+            locationLongitude: 126.9878822,
+        },
+    })
+
+    // posting(user2_theme4(랜드마크)_11)
+    await prisma.post.create({
+        data: {
+            userId: 2,
+            commuThemeId: 4,
+            ImgUrl: "https://res.klook.com/images/fl_lossy.progressive,q_65/c_fill,w_1295,h_862,f_auto/w_80,x_15,y_15,g_south_west,l_klook_water/activities/qwo9klmec2tcooqke60l/%EA%B2%BD%EB%B3%B5%EA%B6%81%EA%B0%80%EC%9D%B4%EB%93%9C%ED%88%AC%EC%96%B4.jpg",
+            title: "경복궁에서 한국 역사 체험하기",
+            text: "경복궁에서 한국 역사를 체험해보세요. 아름다운 건물과 정원을 구경하면서 한국의 역사와 문화를 배울 수 있습니다.",
+            numberOfHearts: 180,
+            locationName: "사직로 161",
+            locationLatitude: 37.5788408,
+            locationLongitude: 126.9770162,
+        },
+    })
+
+    // posting(user3_theme4(랜드마크)_12)
+    await prisma.post.create({
+        data: {
+            userId: 3,
+            commuThemeId: 4,
+            ImgUrl: "http://www.k-heritage.tv/jnrepo/mig/upload/itm/contents/12227/%EA%B2%BD%EC%A3%BC%20%EB%B6%88%EA%B5%AD%EC%82%AC.jpg",
+            title: "불국사에서 고즈넉한 시간 보내기",
+            text: "불국사에서 고즈넉한 시간을 보내보세요. 아름다운 사찰과 국보급 유물을 감상하며 마음을 정화해보세요.",
+            numberOfHearts: 178,
+            locationName: "경강로 2591",
+            locationLatitude: 37.7733758,
+            locationLongitude: 128.9427571,
+        },
+    })
+
+    // posting(user4_theme4(랜드 마크)_13)
     await prisma.post.create({
         data: {
             userId: 4,
@@ -372,21 +387,6 @@ async function main() {
             locationName: "석굴암길 1-12",
             locationLatitude: 37.7004246,
             locationLongitude: 126.9717379,
-        },
-    })
-
-    // posting(user1_theme3(랜드마크)_13)
-    await prisma.post.create({
-        data: {
-            userId: 1,
-            commuThemeId: 3,
-            ImgUrl: "https://tse2.mm.bing.net/th?id=OIP.5rbcYgrB7xbajtWluYU1OAHaHa&pid=Api&P=0",
-            title: "한국의 대표적인 랜드마크, 남산서울타워",
-            text: "남산서울타워는 한국에서 가장 높은 타워 중 하나입니다. 낮과 밤 모두 아름다운 경관을 감상할 수 있으며, 전망대에서 서울의 도시scape를 볼 수 있습니다.",
-            numberOfHearts: 0,
-            locationName: "남산공원길 105",
-            locationLatitude: 37.5510548,
-            locationLongitude: 126.9878822,
         },
     })
 
@@ -419,6 +419,7 @@ async function main() {
             locationLongitude: 127.5394563,
         },
     })
+
 }
 
 main()
